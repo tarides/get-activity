8de2d374933ff3ac05874ee62f5e956843f30d69
Guillaume Petiot <guillaume@tarides.com>
2024-03-11 16:48:18 +0000

    typo

--- lib/user.mli
+++ lib/user.mli
@@ -1,7 +1,7 @@
 type t = Viewer | User of string
 
 val query : Format.formatter -> t -> unit
-(** Prints graphql query fragment that request the given user. *)
+(** Prints graphql query fragment that requests the given user. *)
 
 val response_field : t -> string
-(** The field in the grphql response that contains the user fields. *)
+(** The field in the graphql response that contains the user fields. *)
